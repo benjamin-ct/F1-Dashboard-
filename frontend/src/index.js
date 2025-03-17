3f91eac2452e528aabbc1f876b7b84c5a96e408e
benjamin-ct <31733096+benjamin-ct@users.noreply.github.com>
2025-03-17 11:59:42 +0100

    Create index.js

---
+++ frontend/src/index.js
@@ -0,0 +1,8 @@
+// frontend/src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css'; // Optionnel : pour ajouter du style global
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
